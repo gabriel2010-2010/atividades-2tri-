de30e8aced0a2c8e9cfbc3f09a2c99fe5f993df5
gabriel2010-2010 <gabriel.gomes.francisco@escola.pr.gov.br>
2026-06-26 14:53:36 +0000

    atv 11 pronta

--- back/atv11.js
+++ back/atv11.js
@@ -10,6 +10,15 @@ app.get('/equipe', (req, res) => {
 app.get('/contato', (req, res) => {
   res.send('numero de celular');
 });
+pp.get('/escolha', (req, res) => {
+  res.send('peça de roupa ');
+});
+app.get('/aluno', (req, res) => {
+  res.send('nome');
+});
+app.get('/dia', (req, res) => {
+  res.send('data');
+});
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
